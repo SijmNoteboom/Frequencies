dd1f428899ff39deca37e3641248671959990c1e
SijmNoteboom <sijmnoteboom95@gmail.com>
2020-01-27 22:25:07 -0500

    Open GUI window

--- GUI.py
+++ GUI.py
@@ -1,1 +1,15 @@
+import tkinter as tk
 
+# GUI general setup
+win = tk.Tk()
+
+win.title('Sound Generator')
+win.geometry("1200x700+200+50")
+
+Label_Hz = tk.Label(win, text='Frequency:').grid(row=0)
+frequency_input = tk.Entry(win, width=10)
+frequency_input.grid(column=1, row=0)
+Button_ok = tk.Button(win, text='OK').grid(row=0)
+
+
+win.mainloop()
